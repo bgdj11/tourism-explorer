1ac57700bf5637b742392a677970c8cf4c5be8af
unknown <matejastefanovic214@gmail.com>
2024-10-16 18:40:19 +0200

    feat; added account entity, enabled administrators to see all accounts as well as block/unblock them

--- Explorer/src/app/feature-modules/administration/administration.module.ts
+++ Explorer/src/app/feature-modules/administration/administration.module.ts
@@ -4,13 +4,15 @@ import { EquipmentFormComponent } from './equipment-form/equipment-form.componen
 import { EquipmentComponent } from './equipment/equipment.component';
 import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AccountsComponent } from './accounts/accounts.component';
 
 
 
 @NgModule({
   declarations: [
     EquipmentFormComponent,
-    EquipmentComponent
+    EquipmentComponent,
+    AccountsComponent
   ],
   imports: [
     CommonModule,
--- Explorer/src/app/feature-modules/administration/administration.service.ts
+++ Explorer/src/app/feature-modules/administration/administration.service.ts
@@ -4,6 +4,7 @@ import { Equipment } from './model/equipment.model';
 import { environment } from 'src/env/environment';
 import { Observable } from 'rxjs';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { Account } from './model/account.model';
 
 @Injectable({
   providedIn: 'root'
@@ -28,4 +29,10 @@ export class AdministrationService {
     return this.http.put<Equipment>(environment.apiHost + 'administration/equipment/' + equipment.id, equipment);
   }
 
+  getAccounts(): Observable<PagedResults<Account>> {
+    return this.http.get<PagedResults<Account>>(environment.apiHost + 'administration/accounts')
+  } 
+  updateAccount(account: Account): Observable<Account> {
+    return this.http.put<Account>(environment.apiHost + 'administration/accounts/' + account.id, account);
+  }
 }
