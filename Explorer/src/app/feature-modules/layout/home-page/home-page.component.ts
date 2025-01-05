08808c9b52b3b6a5a002d67fd603038f72facb69
Biljana <biljanakatanic7@gmail.com>
2025-01-05 13:55:42 +0100

    .

--- Explorer/src/app/feature-modules/layout/home-page/home-page.component.ts
+++ Explorer/src/app/feature-modules/layout/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AppRatingComponent } from '../app-rating/app-rating.component';
 
 @Component({
   selector: 'xp-home-page',
